0e18ece7c949ca372a63edf587925939b03aac35
Philipe <philipesoares@cc.ci.ufpb.br>
2019-08-02 17:53:55 -0300

    feat: adicionando robô de vídeo

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const robots ={
     input: require('./robots/input'),
-    text: require('./robots/text.js'),
+    text: require('./robots/text'),
     state: require('./robots/state'),
-    image: require('./robots/image')
+    image: require('./robots/image'),
+    video: require('./robots/video')
 };
 
 async function start() {
-    // robots.input()
-    // await robots.text()
+    robots.input()
+    await robots.text()
     await robots.image()
-    // console.dir(robots.state.load(), { depth: null })
+    await robots.video()
+    console.dir(robots.state.load(), { depth: null })
 };
 start();
